39f581890f77cf3ae783a3e8f2ab471efe39dc38
iamgarvit <87112688+iamgarvit@users.noreply.github.com>
2023-12-22 11:20:34 +0530

    Add files via upload

---
+++ DSA/C++ codes/Practice/pattern4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,k=1;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int x=0;x<(n-i);x++)
+            cout<<" ";
+        for(int j=k;j<k+i;j++){
+            cout<<i<<" ";
+        }
+        k+=i;
+        cout<<endl;
+    }
+}
